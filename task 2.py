cc53e1005de78ab82f294b012ddfe50fd77e2f72
ASDFGH-ALEX <alexsabu053@gmail.com>
2024-10-18 15:10:21 +0530

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,8 @@
+'''
+Author:Alex Sabu
+Date:18-10-2024
+Describtion:a Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+'''
+str_var="Hello, World"
+str1=str_var[7:13]
+print(str1+" Everyone!")
